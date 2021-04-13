a38e64340b7f9d828d75fe4499647301eb579113
MatthewHeinrich <mheinrich95@gmail.com>
2021-04-13 16:36:35 -0500

    something funky is happeing with map

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -2,12 +2,25 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+const encryptLib = require('../modules/encryption');
+const userStrategy = require('../strategies/user.strategy');
+
+router.get('/', rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
   console.log('req.user:', req.user);
+  res.send(req.user);
+  if(req.user.clearance_level <= 4){
+    return
+  } else {
+    
+  }
 
   pool
-    .query(`SELECT * FROM "secret";`)
+    .query(`SELECT * FROM "secret"
+            WHERE "secret".secrecy_level >= 10;`)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
